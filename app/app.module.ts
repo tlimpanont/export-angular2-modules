d4a9c06ad76d75cb07a9c7790f7694b1f549577d
Theuy Limpanont <info@theuy.nl>
2016-09-14 19:57:25 +0200

    refactor(app.module): fix tslint errors

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,8 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent}  from './app.component';
 import {FormsModule} from '@angular/forms';
-import {MyCustomInputModule} from "./my-custom-intput.component";
-import {ComponentsRootModule, ComponentsModule} from "./components.module";
+import {ComponentsModule} from './components.module';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, ComponentsModule],
